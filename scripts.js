85ba13689f7523ebf814a1d042a7382ebde284ed
Ralph Ellis <175041570+Skubicus@users.noreply.github.com>
2024-10-23 17:05:30 +0100

    Added buttons to mainpage to link to projects

---
+++ scripts.js
@@ -0,0 +1,15 @@
+document.getElementById('linkStyleGuide').addEventListener('click', function() {
+  window.location.href = './styleguide/guide.html'; // Path to the desired file
+});
+
+document.getElementById('linkAniTech').addEventListener('click', function() {
+  window.location.href = './retailwebsite/index.html'; // Path to the desired file
+});
+
+document.getElementById('linkTeaCozy').addEventListener('click', function() {
+  window.location.href = './teacozy/theteacozy.html'; // Path to the desired file
+});
+
+document.getElementById('linkfotomatic').addEventListener('click', function() {
+  window.location.href = './fotomatic/index.html'; // Path to the desired file
+});
