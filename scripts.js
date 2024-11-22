2652de383305a7f5c435c313cf9cafc024b50560
Ralph Ellis <175041570+Skubicus@users.noreply.github.com>
2024-11-22 13:32:46 +0000

    fixed null error in scripts.js that prevented the subsequent links from working.

--- scripts.js
+++ scripts.js
@@ -6,9 +6,11 @@ document.getElementById('linkAniTech').addEventListener('click', function() {
   window.location.href = './retailwebsite/index.html'; // Path to the desired file
 });
 
+/* 
 document.getElementById('linkTeaCozy').addEventListener('click', function() {
   window.location.href = './teacozy/theteacozy.html'; // Path to the desired file
 });
+*/
 
 document.getElementById('linkfotomatic').addEventListener('click', function() {
   window.location.href = './fotomatic/index.html'; // Path to the desired file
